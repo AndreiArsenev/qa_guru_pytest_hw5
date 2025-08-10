df6ac7f8558516e1749d4c0924a8ae1c009c2583
Andrei Arsenev <3845400@gmail.com>
2025-08-11 00:54:30 +0300

    Отредактировал тест, сделал более компактным

--- test.py
+++ test.py
@@ -3,18 +3,18 @@ from selene import browser, be, have
 
 
 def test_fill_form():
-    browser.element('[id="firstName"]').type("Andrei")
-    browser.element('[id="lastName"]').type("Arseniev")
-    browser.element('[id="userEmail"]').type("Andrei@gmail.com")
+    browser.element('#firstName').type("Andrei")
+    browser.element('#lastName').type("Arseniev")
+    browser.element('#userEmail').type("Andrei@gmail.com")
     browser.element('label[for="gender-radio-1"]').click()
-    browser.element('[id="userNumber"]').type("9113845400")
+    browser.element('#userNumber').type("9113845400")
     browser.element('#dateOfBirthInput').click()
     browser.element('.react-datepicker__month-select').click()
     browser.element('option[value="8"]').should(be.visible).click()
     browser.element('.react-datepicker__year-select').click()
     browser.element('option[value="1988"]').should(be.visible).click()
     browser.element('.react-datepicker__day--020').click()
-    browser.element('#subjectsInput').should(be.blank).type('English').press_enter()
+    browser.element('#subjectsInput').type("English").press_enter()
     browser.element('label[for ="hobbies-checkbox-1"]').click()
     browser.element('#uploadPicture').send_keys(os.path.abspath('file.txt'))
     browser.element('#currentAddress').type('My address')
@@ -22,7 +22,7 @@ def test_fill_form():
     browser.element('#city').click().element('#react-select-4-option-0').click()
     browser.element('#submit').click()
 
-    browser.element('[id = "example-modal-sizes-title-lg"]').should(have.text('Thanks for submitting the form'))
+    browser.element('#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
 
     browser.all('.table-responsive td:nth-child(2)').should(have.texts(
 
